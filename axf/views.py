d3b38cea3bcde6b6420ea612bae271ffb9a595fe
zyz <zyz@1000phone.com>
2018-11-02 10:29:45 +0800

    爱鲜蜂-首页完成

--- axf/views.py
+++ axf/views.py
@@ -1,11 +1,13 @@
+import random
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from axf.models import Wheel, Nav, Mustbuy
+from axf.models import Wheel, Nav, Mustbuy, Shop, MainShow
 
 
 def home(request):  # 首页
-
     # 轮播图数据
     wheels = Wheel.objects.all()
 
@@ -15,10 +17,25 @@ def home(request):  # 首页
     # 每日必购
     mustbuys = Mustbuy.objects.all()
 
+    # 商品部分
+    shopList = Shop.objects.all()
+    shophead = shopList[0]
+    shoptab = shopList[1:3]
+    shopclass = shopList[3:7]
+    shopcommend = shopList[7:11]
+
+    # 商品主体内容
+    mainshows = MainShow.objects.all()
+
     data = {
         'wheels':wheels,
         'navs':navs,
         'mustbuys': mustbuys,
+        'shophead':shophead,
+        'shoptab':shoptab,
+        'shopclass':shopclass,
+        'shopcommend':shopcommend,
+        'mainshows':mainshows
     }
 
     return render(request, 'home/home.html', context=data)
